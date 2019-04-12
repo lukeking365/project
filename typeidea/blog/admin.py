f037ab36be25915195418545794c3e83f319698c
义兴 王 <yixingwang@msn.com>
2019-04-12 11:41:41 +0800

    commit

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -3,26 +3,19 @@ from django.urls import reverse
 from django.utils.html import format_html
 
 from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.custom_site import custom_site
 
-class CategoryOwnerFilter(admin.SimpleListFilter):
-    # 自定义过滤器只展示当前用户分类
-    title = '分类过滤器'
-    parameter_name = 'owner_category'
-
-    def lookup(self, request, model_admin):
-        return Category.objects.filter(owner = request.user).values_list('id','name')
-    
-    def queryset(self, request, queryset):
-        category_id = self.value()
-        if category_id:
-            return queryset.filter(category_id=self.value())
-        return queryset
-
+class PostInline(admin.TabularInline):
+    fields = ('title','desc')
+    extra = 1 #控制额外多几个
+    model = Post
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
+    inlines = [PostInline,]
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
@@ -42,8 +35,25 @@ class TagAdmin(admin.ModelAdmin):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
-@admin.register(Post)
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    # 自定义过滤器只展示当前用户分类
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+ 
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner = request.user).values_list('id','name')
+    
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = [
         'title', 'category', 'status',
         'created_time', 'operator'
@@ -58,23 +68,53 @@ class PostAdmin(admin.ModelAdmin):
 
     # 编辑页面
     save_on_top = True
+    exclude = ('owner',)
 
-    fields = (
-        ('category','title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    # fields = (
+    #     ('category','title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+    
+    fieldsets = (
+        ('基础配置',{
+            'description':'基础配置描述',
+            'fields':(
+                ('title','category'),'status',
+            ),
+        }),
+        ('内容',{
+            'fields':(
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息',{
+            'classes':('collapse',),
+            'fields':('tag',),
+        }),
     )
-
+    
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,)) #admin 改为cus_admin
         )
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+    
+    # class Media:
+    #     css = {
+    #         'all':("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
+    #     }
+    #     js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js",)
 
