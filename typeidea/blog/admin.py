ebfd94af9305cae84cd2f4b392ce92385940657c
义兴 王 <yixingwang@msn.com>
2019-04-19 15:34:25 +0800

    commit

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,43 +1,46 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+import django.contrib.sites.requests
+from django.contrib.auth import get_permission_codename
+from django.contrib.admin.models import LogEntry
 
-from .models import Post, Category, Tag
 from .adminforms import PostAdminForm
+from .models import Post, Category, Tag
+from typeidea.base_admin import BaseOwnerAdmin
 from typeidea.custom_site import custom_site
 
-class PostInline(admin.TabularInline):
+
+
+class PostInline(admin.TabularInline): #可以选择继承自admin.StackedInline,以获取不同的展示样式。
     fields = ('title','desc')
     extra = 1 #控制额外多几个
     model = Post
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin): # 原为继承admin.ModelAdmin
+    inlines = [PostInline,]
     list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
-    inlines = [PostInline,]
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+    
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(CategoryAdmin, self).save_model(request, obj, form, change)
     
     def post_count(self, obj):
         return obj.post_set.count()
     post_count.short_description = '文章数量'
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
-    
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
-    # 自定义过滤器只展示当前用户分类
+    ''' 自定义过滤器只展示当前用户分类 '''
+
     title = '分类过滤器'
     parameter_name = 'owner_category'
  
@@ -51,24 +54,27 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
+# PERMISSION_API = 'http://permission.sso.com/has_perm?user={}&perm_code={}' #虚拟授权网站
+
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'operator'
+        'created_time','owner', 'operator'
     ]
     list_display_links = []
 
-    list_filter = [CategoryOwnerFilter]
+    list_filter = [CategoryOwnerFilter,]
     search_fields = ['title', 'category__name']
+    save_on_top = True
 
     actions_on_top = True
     actions_on_bottom = True
 
     # 编辑页面
     save_on_top = True
-    exclude = ('owner',)
+    exclude = ['owner',]
 
     # fields = (
     #     ('category','title'),
@@ -82,7 +88,8 @@ class PostAdmin(admin.ModelAdmin):
         ('基础配置',{
             'description':'基础配置描述',
             'fields':(
-                ('title','category'),'status',
+                ('title','category'),
+                'status',
             ),
         }),
         ('内容',{
@@ -92,10 +99,12 @@ class PostAdmin(admin.ModelAdmin):
             ),
         }),
         ('额外信息',{
-            'classes':('collapse',),
+            'classes':('wide',),
             'fields':('tag',),
         }),
     )
+    # filter_horizontal = ('tag',)
+    filter_vertical = ('tag',)
     
     def operator(self, obj):
         return format_html(
@@ -103,18 +112,23 @@ class PostAdmin(admin.ModelAdmin):
             reverse('cus_admin:blog_post_change', args=(obj.id,)) #admin 改为cus_admin
         )
     operator.short_description = '操作'
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
-
-    def get_queryset(self, request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
     
-    # class Media:
-    #     css = {
-    #         'all':("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
-    #     }
-    #     js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js",)
+    # def has_add_permission(self, request):
+    #     opts =self.opts
+    #     codename = get_permission_codename('add', opts)
+    #     perm_code = "%s.%s"%(opts.app_label, codename)
+    #     resp = request.get(PERMISSION_API.format(request.user.username, perm_code))
+    #     if resp.status_code ==200:
+    #         return True
+    #     else:
+    #         return False
 
+    class Media:
+        css = {
+            'all':("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
+        }
+        js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js",)
+
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Comment
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+
+@admin.register(Comment, site=custom_site)
+class CommentAdmin(BaseOwnerAdmin):
     list_display = ('target', 'nickname', 'content', 'website', 'created_time')
