09029842cd1c1bc61cb3839bb10e671060246a2e
义兴 王 <yixingwang@msn.com>
2019-04-25 10:44:39 +0800

    commit

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -17,16 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from blog.views import post_list, post_detail
-# from config.views import links
+from config.views import links
 from typeidea.custom_site import custom_site
 
 urlpatterns = [
     #url(r'^category/(?P<category_id>\d+)/$', post_list,{'example':'nop'}, name='category_list'),
-    url(r'^$', post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    url(r'^post/(?P<post_id>\d+).html$', post_detail),
-    # url(r'^links/$', links),
-    url(r'^super_admin/', admin.site.urls),
-    url(r'^admin/', custom_site.urls),
+    url(r'^$', post_list, name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    url(r'^links/$', links, name='links'),
+    url(r'^super_admin/', admin.site.urls, name='super-admin'),
+    url(r'^admin/', custom_site.urls, name='admin'),
 ]
